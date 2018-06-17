c4a3f9e6e06f28db79776dfb8bb7424b7b33e8a9
Yahia Refaiea <YahiaRefaiea@gmail.com>
2018-06-17 11:11:18 +0300

    finalizing router.js

--- application/babel/organisms/router.js
+++ application/babel/organisms/router.js
@@ -3,33 +3,33 @@ var Router = {
 
 	//	ROUTE
 	route: function(location, callback) {
-		// Clerk.wait()
-		//
-		// var session = Auth.session()
-		// var location = Router.location(location)
-		//
-		// //	UNAUTHORIZED
+		Clerk.wait()
+
+		var session = Auth.session()
+		var location = Router.location(location)
+
+		//	UNAUTHORIZED
 		// if(session == null)
 		// 	Router.unauthorized(location)
-		//
-		// //	APPLICANT
-		// else if(session.permission == 'applicant')
-		// 	Router.applicant(location)
-		//
-		// //	COMPANY
-		// else if(session.permission == 'company')
-		// 	Router.company(location)
-		//
-		// //	CALLBACK
-		// setTimeout(function() {
-		// 	Clerk.stop(function() {
-		// 		Router.updateWrapper()
-		// 		Router.updateLocation(location)
-		//
-		// 		if(typeof callback === 'function' && callback)
-		// 			callback()
-		// 	})
-		// }, 200)
+
+		//	CALLBACK
+		setTimeout(function() {
+			Clerk.stop(function() {
+				Router.updateWrapper()
+			  window.location.hash = location
+
+				if(typeof callback === 'function' && callback)
+					callback()
+			})
+		}, 200)
+	},
+
+	//	LOCATION
+	location: function(location) {
+		if(location === undefined)
+			location = window.location.hash
+
+		return location.replace('#', '')
 	},
 
 	//	SHIFT
@@ -41,22 +41,6 @@ var Router = {
 				Router.updateWrapper(push, pull)
 			})
 		}, 200)
-	},
-
-	//	PROCESS LOCATION
-	processLocation: function(location) {
-		// if(location == undefined)
-		// 	location = window.location.hash
-		//
-		// return location.split('#')[0]
-	},
-
-	//	UPDATE LOCATION
-	updateLocation: function(location) {
-		// if(location == '')
-		// 	window.location.href.split('#')[0]
-		// else
-		// 	window.location.hash = location
 	},
 
 	//	UPDATE WRAPPER
