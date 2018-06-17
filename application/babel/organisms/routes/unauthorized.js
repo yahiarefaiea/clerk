a8cf132639c9b2e979cb33e1d312bcd6ce2f84b5
Yahia Refaiea <YahiaRefaiea@gmail.com>
2018-06-17 12:18:17 +0300

    updated unauthorized

--- application/babel/organisms/routes/unauthorized.js
+++ application/babel/organisms/routes/unauthorized.js
@@ -1,3 +1,16 @@
 Router.unauthorized = function(location, callback) {
+  location = Router.location.split('/')
+
+  Router.wrapper = ['unauthorized']
   console.log('session is: unauthorized')
+
+  if(location[0] == '' || location[0] == undefined) {
+    Router.push('auth intro')
+
+    setTimeout(function() {
+      Router.updateWrapper('switch', 'intro')
+    }, 5000)
+  }
+
+  callback()
 }
