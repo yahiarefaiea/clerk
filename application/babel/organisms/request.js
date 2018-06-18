6b58d21bb65842807dbea0dfc7c5e6827ca5b8f3
Yahia Refaiea <YahiaRefaiea@gmail.com>
2018-06-18 11:52:33 +0300

    update fetch() in request.js

--- application/babel/organisms/request.js
+++ application/babel/organisms/request.js
@@ -1,14 +1,31 @@
 var Request = {
-  data: {},
 
   //  FETCH
   fetch: function(data, callback) {
+    $.ajax({
+      type: 'POST',
+      url: 'includes/php/file.php',
+      data: {data: JSON.stringify(data)},
+      dataType: 'json',
 
+      //  ERROR
+      error: function(XMLHttpRequest, textStatus, errorThrown) {
+        Router.error()
+      },
+
+      //  SUCCESS
+      success: function(data) {
+        Request.inject(data, callback)
+      }
+    })
   },
 
   //  INJECT
   inject: function(data, callback) {
+    console.log(data)
 
+    if(typeof callback === 'function' && callback)
+      callback()
   },
 
   //  SUBMIT
