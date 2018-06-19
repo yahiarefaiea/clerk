4ce9f8024e6a634eceb5b8ed55884aca4078df71
Yahia Refaiea <YahiaRefaiea@gmail.com>
2018-06-19 11:07:54 +0300

    update  applicant.js router

--- application/babel/organisms/routes/applicant.js
+++ application/babel/organisms/routes/applicant.js
@@ -1,3 +1,53 @@
 Router.applicant = function(callback) {
-  console.log('session is: applicant')
+  var location = Router.location.split('/').filter(Boolean)
+  Router.wrapper = ['applicant']
+
+  //  AUTH
+  if(location[0] === undefined) {
+    Router.push('auth intro')
+
+    setTimeout(function() {
+      Router.updateWrapper('switch', 'intro')
+    }, 1500 * 4)
+  }
+
+  //  APP
+  else {
+    Router.push('app')
+
+    //  VACANCIES
+    if(location[0] == 'vacancies') {
+      if(location[1] === undefined) {
+        // TODO: Add a fetch request
+        Router.push('vacancies')
+      }
+
+      else Router.push('notFound')
+    }
+
+    //  COMPANY
+    else {
+      var company = location[0]
+
+      if(location[1] === undefined) {
+        // TODO: Add a fetch request
+        Router.push('company')
+      }
+
+      //  VACANCY
+      else {
+        var vacancy = location[1]
+
+        if(location[2] === undefined) {
+          // TODO: Add a fetch request
+          Router.push('vacancy')
+        }
+
+        else Router.push('notFound')
+      }
+    }
+  }
+
+  //  CALLBACK
+  Router.callback(callback)
 }
