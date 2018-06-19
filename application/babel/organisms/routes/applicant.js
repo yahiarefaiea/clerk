d8bed7c238c78fe3c6bd5655c3d744760787a45b
Yahia Refaiea <YahiaRefaiea@gmail.com>
2018-06-19 14:50:43 +0300

    update applicantJS

--- application/babel/organisms/routes/applicant.js
+++ application/babel/organisms/routes/applicant.js
@@ -1,59 +1,59 @@
 Router.applicant = function(callback) {
+  if(Router.location == '') Router.location = 'personality'
   var location = Router.location.split('/').filter(Boolean)
   Router.wrapper = ['authorizedAsApplicant app']
 
   //  PERSONALITY
-  if(location[0] === undefined || location[0] == 'personality') {
-    Router.push('applicant personality')
+  if(location[0] == 'personality') {
+    Router.push('personality')
 
     //  INSIGHTS
     if(location[1] === undefined) {
       // TODO: Add a fetch request
-      Router.push('insights')
+      Router.push('applicant insights')
     }
 
     //  UPDATE
+    else if(location[1] == 'update' && location[2] === undefined) {
+      // TODO: Add a fetch request
+      // Take care.. this one is a bit different.
+      Router.push('update')
+    }
+
+    else Router.push('notFound')
+  }
+
+  //  VACANCIES
+  else if(location[0] == 'vacancies') {
+    if(location[1] === undefined) {
+      // TODO: Add a fetch request
+      Router.push('vacancies')
+    }
+
+    else Router.push('notFound')
+  }
+
+  //  COMPANY
+  else {
+    var company = location[0]
+
+    if(location[1] === undefined) {
+      // TODO: Add a fetch request
+      Router.push('company')
+    }
+
+    //  VACANCY
     else {
+      var vacancy = location[1]
+
       if(location[2] === undefined) {
         // TODO: Add a fetch request
-        Router.push('update')
+        Router.push('vacancy')
       }
 
       else Router.push('notFound')
     }
   }
-
-    //  VACANCIES
-    else if(location[0] == 'vacancies') {
-      if(location[1] === undefined) {
-        // TODO: Add a fetch request
-        Router.push('vacancies')
-      }
-
-      else Router.push('notFound')
-    }
-
-    //  COMPANY
-    else {
-      var company = location[0]
-
-      if(location[1] === undefined) {
-        // TODO: Add a fetch request
-        Router.push('company')
-      }
-
-      //  VACANCY
-      else {
-        var vacancy = location[1]
-
-        if(location[2] === undefined) {
-          // TODO: Add a fetch request
-          Router.push('vacancy')
-        }
-
-        else Router.push('notFound')
-      }
-    }
 
   //  CALLBACK
   Router.callback(callback)
