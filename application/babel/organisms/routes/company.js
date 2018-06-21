d0ae6242563fd76f83b0b58130ec23444314ff12
Yahia Refaiea <YahiaRefaiea@gmail.com>
2018-06-21 18:14:13 +0300

    updated company's permissions

--- application/babel/organisms/routes/company.js
+++ application/babel/organisms/routes/company.js
@@ -1,3 +1,55 @@
 Router.company = function(callback) {
-  console.log('session is: company')
+  if(Router.location == '') Router.location = 'personality'
+  var location = Router.location.split('/').filter(Boolean)
+  Router.wrapper = ['authorizedAsCompany app']
+
+  //  PERSONALITY
+  if(location[0] == 'personality') {
+
+    //  INSIGHTS
+    if(location[1] === undefined) {
+      // TODO: Add a fetch request
+      Router.push('personality applicant insights')
+    }
+
+    //  UPDATE
+    else if(location[1] == 'update' && location[2] === undefined) {
+      // TODO: Add a fetch request
+      // Take care.. this one is a bit different.
+      Router.push('personality update')
+    }
+
+    else Router.push('notFound')
+  }
+
+  //  VACANCIES
+  else if(location[0] == 'vacancies' && location[1] === undefined) {
+    // TODO: Add a fetch request
+    Router.push('vacancies')
+  }
+
+  //  COMPANY
+  else {
+    var company = location[0]
+
+    if(location[1] === undefined) {
+      // TODO: Add a fetch request
+      Router.push('company')
+    }
+
+    //  VACANCY
+    else {
+      var vacancy = location[1]
+
+      if(location[2] === undefined) {
+        // TODO: Add a fetch request
+        Router.push('vacancy')
+      }
+
+      else Router.push('notFound')
+    }
+  }
+
+  //  CALLBACK
+  Router.callback(callback)
 }
