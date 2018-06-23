08bfadbae3a5920e1b9e1b73ca5165656aa6a304
Yahia Refaiea <YahiaRefaiea@gmail.com>
2018-06-23 06:18:13 +0300

    cleaning up request.js

--- application/babel/organisms/request.js
+++ application/babel/organisms/request.js
@@ -3,53 +3,31 @@ var Request = {
 
   //  CLAIM
   claim: function(url, onSuccess, onError, data, method) {
-    if(data === undefined) data = null
-    if(method === undefined) method = 'POST'
 
-    //  AJAX
-    $.ajax({
-      type: method,
-      url: Request.origin + url,
-      data: {data: JSON.stringify(data)},
-      dataType: 'json',
-
-      //  ERROR
-      error: function(XMLHttpRequest, textStatus, errorThrown) {
-        if(typeof onError === 'function' && onError)
-          onError()
-        else Router.push('notFound')
-      },
-
-      //  SUCCESS
-      success: function(response) {
-        if(typeof onSuccess === 'function' && onSuccess)
-          onSuccess(response)
-      }
-    })
   },
 
   //  FETCH
   fetch: function(template, url, callback) {
-    Request.claim(url, function(response) {
-      Request.inject(template, response, callback)
-    })
+
   },
 
   //  INJECT
   inject: function(template, data, callback) {
+
   },
 
   //  INJECT
   separate: function() {
+
   },
 
   //  SUBMIT
-  // submit: function(data, callback) {
-  push: function(parent, callback) {
+  submit: function(template, callback) {
+    
   },
 
   //  PREPARE
-  // prepare: function(data, callback) {
-  prepare: function(parent) {
+  prepare: function(template, data, callback) {
+
   }
 }
