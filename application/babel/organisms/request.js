440239f72b5b63bc5f8931cade43b3b46b4666c9
Yahia Refaiea <YahiaRefaiea@gmail.com>
2018-06-23 06:10:01 +0300

    Cleaning up auth.js and request.js

--- application/babel/organisms/request.js
+++ application/babel/organisms/request.js
@@ -1,150 +1,55 @@
 var Request = {
-  baseUrl: 'http://beta.clerk.technology/v2/',
+  origin: 'http://beta.clerk.technology/v2/',
 
-  //  FETCH
-  fetch: function(template, data, url, callback) {
+  //  CLAIM
+  claim: function(url, onSuccess, onError, data, method) {
+    if(data === undefined) data = null
+    if(method === undefined) method = 'POST'
+
+    //  AJAX
     $.ajax({
-      type: 'POST',
-      url: Request.baseUrl + url,
+      type: method,
+      url: Request.origin + url,
       data: {data: JSON.stringify(data)},
       dataType: 'json',
 
       //  ERROR
       error: function(XMLHttpRequest, textStatus, errorThrown) {
-        Router.error()
+        if(typeof onError === 'function' && onError)
+          onError()
+        else Router.push('notFound')
       },
 
       //  SUCCESS
-      success: function(data) {
+      success: function(response) {
+        if(typeof onSuccess === 'function' && onSuccess)
+          onSuccess(response)
       }
     })
-    // Request.inject(template, data, callback)
+  },
+
+  //  FETCH
+  fetch: function(template, url, callback) {
+    Request.claim(url, function(response) {
+      Request.inject(template, response, callback)
+    })
   },
 
   //  INJECT
   inject: function(template, data, callback) {
-    function findObject(o, s) {
-      //  convert indexes to properties
-      s = s.replace(/\[(\w+)\]/g, '.$1')
-      //  strip a leading dot
-      s = s.replace(/^\./, '')
-      var a = s.split('.')
-      for (var i = 0, n = a.length; i < n; ++i) {
-        var k = a[i]
-        if (k in o) {
-          o = o[k]
-        } else {
-          return
-        }
-      }
-      return o
-    }
+  },
 
-    // clear everything that has 'data-inject'
-    // $()
-
-    var elements = $('.template[data-template="' + template + '"]').find('[data-inject]')
-
-    for (var i = 0; i < elements.length; i++) {
-      var element = $(elements[i])
-      var type = $(element).attr('data-inject')
-      // change type to key
-
-      if(element.hasClass('links')) {
-        element.empty()
-        if(!Array.isArray(data)) data = findObject(data, type)
-
-        for (var j = 0; j < data.length; j++) {
-          var href, title, subtitle
-          if(element.closest('.template[data-template="applicants"]').length != 0) {
-            href = 'vacancy/' + data[j].VacancyID + '/applicants/' + data[j].UserID
-            title = data[j].FirstName + ' ' + data[j].SurName
-            subtitle = data[j].Major
-          }
-          else {
-            if(Mock.Session) {
-              console.log('passing..')
-              if(Mock.Session.UserID == data[j].CompanyID) {
-                href = 'vacancy/' + data[j].VacancyID
-                title = data[j].Title
-                subtitle = data[j].Hours + ' at ' + data[j].CompanyName
-              }
-            }
-            else {
-              href = data[j].CompanyID + '/' + data[j].VacancyID
-              title = data[j].Title
-              subtitle = data[j].Hours + ' at ' + data[j].CompanyName
-            }
-          }
-
-          var linkGroup = $('<a class="link-group router" href="#' + href + '" title="' + title + '"></a>')
-          linkGroup.append('<div class="h3"><span>' + title + '</span></div>')
-          linkGroup.append('<div class="p"><span>' + subtitle + '</span></div>')
-
-          linkGroup.wrap('<li></li>')
-          element.append(linkGroup.parent())
-        }
-      }
-
-      else if(element.hasClass('list')) {
-        element.empty()
-        var items = findObject(data, type)
-
-        for (var j = 0; j < items.length; j++) {
-          element.append('<li>' + items[j] + '</li>')
-        }
-      }
-
-      else if(element.hasClass('link')) {
-        if(element.hasClass('router')) {
-          // var linkText = data[type].replace('www.', '').split('://')[1]
-          element.text(data[type])
-          element.attr('title', data[type])
-          // element.attr('href', data[type])
-
-          // element.text(data.CompanyID)
-          // element.attr('title', data.CompanyID)
-          var router = element.attr('data-router')
-          if(router == 'company') element.attr('href', data.CompanyID)
-        }
-        else {
-          var linkText = data[type].replace('www.', '').split('://')[1]
-          element.text(linkText)
-          element.attr('title', linkText)
-          element.attr('href', data[type])
-        }
-      }
-
-      else {
-        if(type.includes('.')) element.text(findObject(data, type))
-        else element.text(data[type])
-      }
-    }
-
-    tippyInit()
-    Router.push(template)
-    if(typeof callback === 'function' && callback)
-      callback()
+  //  INJECT
+  separate: function() {
   },
 
   //  SUBMIT
   // submit: function(data, callback) {
   push: function(parent, callback) {
-    Prepare.prepare(parent, function() {})
-
-    callback()
-    Prepare.data = {}
   },
 
   //  PREPARE
   // prepare: function(data, callback) {
   prepare: function(parent) {
-    var fields = $(parent).find('input')
-
-    for (i = 0; i < fields.length; i++) {
-      var field = $(fields[i])
-
-      Prepare.data[field.attr('name')] = field.val()
-    }
   }
 }
--- application/babel/templates/auth.js
+++ application/babel/templates/auth.js
@@ -1,112 +1,17 @@
 var Auth = {
 
   session: function() {
-    // check session here
-
-    // then return the object
-    return Mock.Session
-    // return {
-    //   permission: 'company',
-    //   user: {
-    //     id: '515'
-    //   }
-    // }
   },
 
-  signedIn: function() {
-    setTimeout(function() {
-      Router.route('', function() {
-        // callback goes here if you wish to put something..
-      })
-		}, 200)
+  authorized: function() {
   },
 
   signIn: function(e) {
-
-    Clerk.wait()
-    var form = '[data-request="signIn"]'
-
-    Prepare.push(form, function() {
-      var data = Prepare.data
-
-      console.log(data)
-
-
-      var success = true
-      if(success == true) {
-        Auth.session = 'user'
-        Auth.user.permission = 'comp'
-        Auth.signedIn()
-      } else {
-        Clerk.stop()
-        console.log('something went wrong..')
-      }
-
-      // $.ajax({
-      //   type: 'POST',
-      //   url: '...',
-      //   data: {d: JSON.stringify(data)},
-      //   dataType: 'json',
-      //   error: function(XMLHttpRequest, textStatus, errorThrown) {
-      //     console.log('error')
-      //   },
-      //   success: function(data) {
-      //     Auth.session = 'user'
-      //     Auth.permission = 'comp'
-      //     Auth.signedIn(data.status)
-      //   }
-      // })
-
-    })
-
-    // e.preventDefault()
   },
 
   signUpAsApplicant: function(e) {
-
-    var baseUrl = 'http://beta.clerk.technology/v2/'
-    var url = 'session.php'
-
-    $.ajax({
-      type: 'POST',
-      url: baseUrl + url,
-      // data: {data: JSON.stringify(data)},
-      dataType: 'json',
-
-      //  ERROR
-      error: function(XMLHttpRequest, textStatus, errorThrown) {
-        // Router.error()
-        console.log(XMLHttpRequest)
-        console.log(textStatus)
-        console.log(errorThrown)
-      },
-
-      //  SUCCESS
-      success: function(data) {
-        console.log(data)
-        return data
-      }
-    })
-
-    // Clerk.wait()
-    // var form = '[data-request="signUpAsApplicant"]'
-    //
-    // Prepare.push(form, function() {
-    //   console.log(Prepare.data)
-    // })
-
-    // e.preventDefault()
   },
 
   signUpAsCompany: function(e) {
-
-    // Clerk.wait()
-    // var form = '[data-request="signUpAsCompany"]'
-    //
-    // Prepare.push(form, function() {
-    //   console.log(Prepare.data)
-    // })
-
-    // e.preventDefault()
   }
 }
