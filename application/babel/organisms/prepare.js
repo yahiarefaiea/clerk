2f1f84cc4c2fdbb9d32613767ddf6f99b61efcd2
Yahia Refaiea <YahiaRefaiea@gmail.com>
2018-06-25 18:00:46 +0300

    push prepare.js

--- application/babel/organisms/prepare.js
+++ application/babel/organisms/prepare.js
@@ -3,6 +3,15 @@ var Prepare = {
 
   //  MAKE
   make: function(form, callback) {
+    var data = {}
+    var fields = $('.form#' + form).find('input')
 
+    for (i = 0; i < fields.length; i++) {
+      var field = $(fields[i])
+      if(field.val() != '')
+        data[field.attr('name')] = field.val()
+    }
+
+    if(typeof callback === 'function' && callback) callback(data)
   }
 }
